14b2cd240d0096c8bf41ea2c2b6812f5210e8113
mabbuprem <mabbupremkumar@gmail.com>
2022-04-17 10:20:41 +0530

    [prem] FEATURE ADDED: ADDED: Calculate Multiple Rides Fare & Min Fare Testing

--- Cab-Invoice-Problems/CabInvoiceGenerator.cs
+++ Cab-Invoice-Problems/CabInvoiceGenerator.cs
@@ -8,7 +8,8 @@ namespace Cab_Invoice_Problem
         const int COST_PER_KM = 10;
         const int COST_PER_MINUTE = 1;
         const int MINIMUM_FARE = 5;
-        public double GetTotalFare(double distance, double time)
+
+        public double GetTotalFareSingleRide(double distance, double time)
         {
             double singleRideFare = distance * COST_PER_KM + time * COST_PER_MINUTE;
             if (singleRideFare < MINIMUM_FARE)
@@ -16,6 +17,15 @@ namespace Cab_Invoice_Problem
                 return MINIMUM_FARE;
             }
             return singleRideFare;
+        }
+        public double GetTotalFareMultipleRide(Ride[] rides)
+        {
+            double multipleRideFare = 0;
+            foreach (Ride ride in rides)
+            {
+                multipleRideFare += GetTotalFareSingleRide(ride.distance, ride.time);
+            }
+            return multipleRideFare;
         }
     }
 }
---
+++ Cab-Invoice-Problems/Ride.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cab_Invoice_Problem
+{
+    public class Ride
+    {
+        public double distance { get; set; }
+        public double time { get; set; }
+
+        public Ride(double distance, double time)
+        {
+            this.distance = distance;
+            this.time = time;
+        }
+    }
+}
